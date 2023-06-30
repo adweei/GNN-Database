28485d9ad98808113b2743215b7980c1f692f56f
adweei11111 <apple871108@gmail.com>
2023-07-01 00:41:13 +0800

    6/30 code update

--- GCN_code/preprosess/elon_graph_construct.py
+++ GCN_code/preprosess/elon_graph_construct.py
@@ -7,18 +7,18 @@ import os
 import dgl
 import json
 
-time_graph_dir = 'D:/GNN/MyResearch-main/MyResearch-main/ElonMusk/2023-02-16/'
+time_graph_dir = 'D:/GCN_Twitter/ElonMusk/2023-02-16/'
 all_base_graph_dir = list()
-id = open(time_graph_dir+'Graph/encoding_table.json')           #把userid給讀入
+id = open(time_graph_dir + 'Graph/encoding_table.json')           #把userid給讀入
 user_id = json.load(id)
 id.close()
-for it in os.scandir(time_graph_dir+'base_graph_for_model/'):
+for it in os.scandir(time_graph_dir + 'base_graph_for_model/'):
     if it.is_dir():
-        all_base_graph_dir.append(it.path+"/")
-popular = pd.read_excel(time_graph_dir+'user_profile/Profile_new.xlsx')
+        all_base_graph_dir.append(it.path + "/")
+popular = pd.read_excel(time_graph_dir + 'user_profile/Profile_new.xlsx')
 popular[['id','popular']] = popular[['id','popular']].astype(str)
 popular = popular[['id','popular']]
-dataset,labels =  load_graphs(time_graph_dir+"Graph/base_graph.bin")
+dataset,labels =  load_graphs(time_graph_dir + 'Graph/base_graph.bin')
 base_graph = dataset[0]
 u,v = base_graph.edges()
 edge_weight = base_graph.edata['weight']
@@ -30,7 +30,7 @@ for per_tweet in range(len(all_base_graph_dir)):#
     tweet_number = all_base_graph_dir[per_tweet].split("/")
     tweet_number = tweet_number[-2]
 
-    label = np.load(time_graph_dir+'label/label_for_'+str(tweet_number)+'.npy')
+    label = np.load(time_graph_dir + 'label/label_for_' + str(tweet_number)+'.npy')
 
     print(base_graph.number_of_nodes())
     print(len(label))
@@ -56,18 +56,18 @@ for per_tweet in range(len(all_base_graph_dir)):#
         break
     base_graph.edata['weight'] = edge_weight
     break
-    for times in range(5):
-        mask = th.rand(base_graph.number_of_nodes())
-        train_mask = mask < 0.8
-        train_mask = train_mask.numpy()
-        test_mask = mask >= 0.8
-        test_mask = test_mask.numpy()
-        if not os.path.exists(Save_train_Path):
-            os.makedirs(Save_train_Path)
-        if not os.path.exists(Save_test_Path):
-            os.makedirs(Save_test_Path)
-        np.save(Save_train_Path+str(times),train_mask)
-        np.save(Save_test_Path+str(times),test_mask)
-    times += 1
-    save_graphs(all_base_graph_dir[per_tweet]+str(tweet_number)+".bin", [base_graph])#將graph的結果輸出   
+    # for times in range(5):
+    #     mask = th.rand(base_graph.number_of_nodes())
+    #     train_mask = mask < 0.8
+    #     train_mask = train_mask.numpy()
+    #     test_mask = mask >= 0.8
+    #     test_mask = test_mask.numpy()
+    #     if not os.path.exists(Save_train_Path):
+    #         os.makedirs(Save_train_Path)
+    #     if not os.path.exists(Save_test_Path):
+    #         os.makedirs(Save_test_Path)
+    #     np.save(Save_train_Path+str(times),train_mask)
+    #     np.save(Save_test_Path+str(times),test_mask)
+    # times += 1
+    # save_graphs(all_base_graph_dir[per_tweet]+str(tweet_number)+".bin", [base_graph])#將graph的結果輸出   
 print(times)
--- GCN_code/preprosess/elon_model_graph_train.py
+++ GCN_code/preprosess/elon_model_graph_train.py
@@ -1,11 +1,22 @@
 import sys
-sys.path.append("D:/GNN/code/gcn")
+sys.path.append("D:/GCN_Twitter/GCN_code/gcn")
+import time
 from model_run import model_run
 
+start = time.time()
+base_graph_for_model_dir = 'D:/GCN_Twitter/ElonMusk/2023-02-16/base_graph_for_model/'
 for number_graph in range(0,27,1):#27
     for batch in range(5):#5
         print('now run graph nubmer: {},round: {}'.format(number_graph,batch))
         model_construct = model_run()
         model_construct.elonmask_dataset_load(number_graph,batch)
         model_construct.run_model()
-        model_construct.model_save('D:/GNN/MyResearch-main/MyResearch-main/ElonMusk/2023-02-16/base_graph_for_model/'+str(number_graph)+'/'+str(batch)+'_round_result_edge_weight_Adjustment',batch) 
+        model_construct.model_save(base_graph_for_model_dir + str(number_graph) + '/' +
+                                    str(batch) + '_round_result_edge_weight_Adjustment',batch) 
+
+end = time.time()
+
+total_time = end - start
+sec = int(total_time % 60)
+min = int(total_time / 60)
+print("執行時間：%d分 %d 秒" %(min, sec))
